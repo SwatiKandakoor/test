371c08df94a423dce44608240ac7a02c291edb30
swatik.xworkz@gmail.com <swatik.xworkz@gmail.com>
2023-01-06 13:24:37 +0530

    status

---
+++ test1.java
@@ -0,0 +1,43 @@
+3)Generalization: Converting a subclass type into a superclass type is called Generalization it is also called as a up casting
+ up cating  is safe because the classes will become more general.
+ specialized: Converting a superclass type into a subclass type is called specialized. A class which is very particular and
+ states the specific details. 
+4)a. A class declared as final can't be extended
+
+b.final calsses:Final class cannot be inherited by any subclass
+  java.lang.string
+  java.lang.math
+  
+c.Delegation can be an alternative to inheritance. Delegation means that you use an object 
+of another class as an instance variable, and forward messages to the instance  
+
+5)java.util.collection
+ comaparble and compator
+ performs the sorting in ascending order
+ 
+6)linked list 
+ it is node based,it slower to read,but faster to manipualtion. they store elements in memory
+ Arraylist 
+ it is indexed based,it is faster to read,but slower to manipualtion 
+ 
+7)Listiterator 
+ list iterator can  be indexed base.
+ iterator
+ it can use list and set 
+ iterator can not be indexed base
+ 
+ 8)it helps to remove the client-side typecasting by enabling you to return a subtype of the overridden method's actual return type.
+ 
+ 9)A Memory Leak is a situation where there are objects present in the heap that are no longer used, 
+ but the garbage collector is unable to remove them from memory, and they're unnecessarily maintained.
+ A memory leak is bad because it blocks memory resources and degrades system performance over time.
+ 
+ 10)Static polymorphism  another name is compile time Polymorphusm,
+ it is a type of polymorphism that collects the information to call a method during compile time.
+ method signture checks the compile time polymorphusm.
+ Dynamic polymorphism  another name is runtime polymorphism,
+ it is a type of polymorphism that collects information to call a method at run time. 
+ jvm checks the runtime polymorphism.
+ 
+ 11)by using Encapsulation
+ 
